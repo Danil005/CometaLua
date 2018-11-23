ce720cc36b2c5db2eab37311516eb4ae9928acfa
Danil005 <commando_danil@mail.ru>
2018-11-24 02:35:11 +0300

    [Fix] Main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,5 @@
 display.setStatusBar( display.HiddenStatusBar)
 math.randomseed( os.time() )
 local composer = require("composer")
-WIDTH = display.contentWidth
-HEIGHT = display.contentHeight
 
-<<<<<<< HEAD
 composer.gotoScene("Scenes.Menu")
-=======
-composer.gotoScene("Scenes.Game_arkada")
->>>>>>> master
