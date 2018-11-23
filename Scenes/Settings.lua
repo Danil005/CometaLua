09ec3508db3948a8d30e518e4714c76efa409c00
Danil005 <commando_danil@mail.ru>
2018-11-24 01:16:05 +0300

    Create Settings Scene

---
+++ Scenes/Settings.lua
@@ -0,0 +1,29 @@
+local composer = require("composer")
+
+local scene = composer.newScene()
+
+local background = nil
+local image_comet = nil
+
+function scene:create(event)
+    local scene_group = self.view
+    background = display.newImageRect( scene_group, "Sprites/background.png",display.contentWidth,display.contentHeight)
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
+    image_comet = display.newImageRect( scene_group,"Sprites/sprite_comet.tif",40,100)
+
+    image_comet.x = display.contentCenterX
+    image_comet.y = display.contentCenterY * 1.5
+end
+
+function scene:show(event)
+end
+
+function scene:hide(event)
+end
+
+scene:addEventListener("create",scene)
+scene:addEventListener("show",scene)
+scene:addEventListener("hide",scene)
+
+return scene
