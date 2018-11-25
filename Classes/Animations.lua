ebd4342edde09231c90280d34b113e30ea425c82
Michael <kozakmikhail@gmail.com>
2018-11-25 09:27:10 +0300

    [update] New loading animations

--- Classes/Animations.lua
+++ Classes/Animations.lua
@@ -1,3 +1,5 @@
+Loading = {}
+
 local loading_options =
 {
   width = 1119,
@@ -7,70 +9,17 @@ local loading_options =
   sheetContentHeight = 1856
 }
 
-local comet_options =
-{
-    width = 1119,
-    height = 1856,
-    numFrames = 5,
-    sheetContentWidth = 5596,
-    sheetContentHeight = 1856
-}
+function Loading:new(x, y, size)
+  obj = {}
+  obj.image = display.newImageRect("Sprites/loading.png", size, size)
+  obj.image.x = x
+  obj.image.y = y
 
-local comet_anim_options =
-{
-  width =330,
-  height = 1702,
-  numFrames = 30,
-  sheetContentWidth = 9900,
-  sheetContentHeight = 1702
-}
+  setmetatable(obj, self)
+  self.__index = self
+  return obj
+end
 
-local front_comet_sheet = graphics.newImageSheet( "images/comet_front.png", comet_options)
-local left_comet_sheet = graphics.newImageSheet( "images/comet_left.png", comet_options)
-local right_comet_sheet = graphics.newImageSheet( "images/comet_right.png", comet_options)
-local forward_comet_sheet = graphics.newImageSheet("images/comet_forward.png", comet_anim_options)
-
-local comet_data =
-{
-  {
-    name = "front",
-    sheet = front_comet_sheet,
-    start = 1,
-    count = 5,
-    time = 800,
-    loopCount = 0
-  },
-  {
-    name = "left",
-    sheet = left_comet_sheet,
-    start = 1,
-    count = 5,
-    time = 800,
-    loopCount = 0
-  },
-  {
-    name = "right",
-    sheet = right_comet_sheet,
-    start = 1,
-    count = 5,
-    time = 300,
-    loopCount = 0
-  },
-  {
-    name = "forward",
-    sheet = forward_comet_sheet,
-    start = 1,
-    count = 30,
-    time = 1000,
-    loopCount = 0
-  }
-}
-
-local comet = display.newSprite(forward_comet_sheet, comet_data);
-comet:setSequence("forward")
-comet.x = 150
-comet.y = 300
-comet:play();
-
-comet:scale(0.2, 0.2);
--- return scene
+function Loading:next_frame()
+  self.image.rotation = self.image.rotation + 3
+end
--- Scenes/Game_arkada.lua
+++ Scenes/Game_arkada.lua
@@ -2,6 +2,7 @@ local composer = require("composer")
 local gravity = require("Classes.Gravity")
 require("Classes.Comet")
 require("Classes.Gravity")
+require("Classes.Animations")
 
 local physics = require( "physics" )
 physics.start()  -- Start the physics engine
@@ -189,8 +190,6 @@ local function enterFrame(event)
     else
         asteroid_group.y = asteroid_group.y + speed_asteroids
     end
-
-
 
     if (asteroid_group.y > HEIGHT*1.2) then
         display.remove(asteroid_group)
